fa5b66e6f6f84b2c6c1ad91c6059adc60b4c8310
wangchi <hiwangchi@gmail.com>
2015-10-23 19:23:13 +0800

    grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,20 @@
+'use strict';
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        less: {
-            compile: {
-                options: {
-                    paths: ['test/css'],
-                    // cleancss: true,
-                    modifyVars: {
-                        imgPath: '"http://www.walkerbe.com/static/images/"',
-                        bgColor: 'red'
-                    }
-                },
-                files: {
-                    'dist/css/style.css': 'src/less/index.less'
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          paths: ['assets/css']
         },
-
-        csslint: {
-            build: {
-                src: ['dist/css/*.css']
-            }
-        },
-        cssmin: {
-            //options: {
-            //    banner: '/*! <%= grunt.template.today("yyyy-mm-dd H:M:s") %> @WalkerBe */'
-            //},
-
-            style: {
-                src: 'dist/css/style.css',
-                dest: 'dist/css/style.min.css'
-            }
-        },
-
-        watch: {
-            scripts: {
-                files: ['less/*.less'],
-                tasks: 'default'
-            }
+        files: {
+          'dist/css/style.css': 'src/less/index.less'
         }
-    });
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-css');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('default', ['less', 'cssmin', 'watch']);
-
+  grunt.registerTask('default', ['less']);
 };
