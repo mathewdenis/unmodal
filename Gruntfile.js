0a8179698842df33791cfa8a0997f093740c6046
wangchi <hiwangchi@gmail.com>
2015-11-26 21:15:38 +0800

    new dialog

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,34 @@
 'use strict';
 module.exports = function (grunt) {
+  var paths = {
+    less: 'src/less/',
+    dist: 'dist/'
+  };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     less: {
       development: {
         options: {
-          paths: ['assets/css']
+          paths: ['assets/css'],
+          cleancss: true
         },
         files: {
-          'dist/css/style.css': 'src/less/index.less'
+          'dist/dialog.css': 'src/less/dialog.less'
         }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/less/*.less'],
+        tasks: 'less'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['less']);
+  grunt.registerTask('default', ['less', 'watch']);
 };
